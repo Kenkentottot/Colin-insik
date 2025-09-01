eea0adc243103327754dd69540ce53a944b052df
Kenkentottot <kenphcool@gmail.com>
2025-09-01 17:57:09 +0800

    v1

--- File/assignment.py
+++ File/assignment.py
@@ -1,12 +1,17 @@
-num1 = int(input("Enter first number:  "))
-num2 = int(input("Enter second number:  "))
-search = int(input("Enter number to search:  "))
-for i in range(1, num1 + 1):
-    for j in range(1, num2 + 1):
-        value = i * j
-        if value == search:
-            print(f"[{value:3}]", end =" ")
-        else:
-            print(f"{value:5}", end =" ")
-    print()
+while True:
+    num1 = int(input("Enter first number:  "))
+    num2 = int(input("Enter second number:  "))
+    search = int(input("Enter number to search:  "))
+    if num1 < 1 or num2 < 1 or search < 1:
+        break
+    else:
+        for i in range(1, num1 + 1):
+            for j in range(1, num2 + 1):
+                value = i * j
+                if value == search:
+                    print(f"[{value:3}]", end =" ")
+                else:
+                    print(f"{value:5}", end =" ")
+            print()
+
     
